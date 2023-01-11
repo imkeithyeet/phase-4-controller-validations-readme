165aab460c232ea08ceb8d808bab97e7f549c693
Keith Ma <keithchezzy@gmail.com>
2023-01-11 00:23:49 -0800

    commit tree 413327ff7e1eb6941e917744560a201e3f2d72f1

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,5 +1,6 @@
 class BirdsController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
 
   # GET /birds
   def index
@@ -9,9 +10,10 @@ class BirdsController < ApplicationController
 
   # POST /birds
   def create
-    bird = Bird.create(bird_params)
+    bird = Bird.create!(bird_params)
     render json: bird, status: :created
   end
+end
 
   # GET /birds/:id
   def show
@@ -22,7 +24,7 @@ class BirdsController < ApplicationController
   # PATCH /birds/:id
   def update
     bird = find_bird
-    bird.update(bird_params)
+    bird.update!(bird_params)
     render json: bird
   end
 
@@ -35,6 +37,10 @@ class BirdsController < ApplicationController
 
   private
 
+  def render_unprocessable_entity_response(invalid)
+    render json: { errors: invalid.record.errors }, status: :unprocessable_entity
+  end
+
   def find_bird
     Bird.find(params[:id])
   end
@@ -46,5 +52,4 @@ class BirdsController < ApplicationController
   def render_not_found_response
     render json: { error: "Bird not found" }, status: :not_found
   end
-
 end
